609aee409b67449e0ca846a5d3311dccfc4a10c4
Dan Prudhomme <dannyprud@yahoo.com>
2025-08-08 11:16:51 -0500

    add skeleton

---
+++ models/dap-translate.js
@@ -0,0 +1,12 @@
+import { pipeline } from "@xenova/transformers";
+import DAPTimer from "@kingdanx/dap-timer";
+
+class DAPTranslate {
+  constructor() {
+    this.languages = new Map();
+  }
+
+  async loadModels() {}
+
+  translate() {}
+}
---
+++ test/dap-translate.test.js
@@ -0,0 +1,2 @@
+import { test, expect } from "bun:test";
+import DAPTranslate from "../models/dap-translate";
